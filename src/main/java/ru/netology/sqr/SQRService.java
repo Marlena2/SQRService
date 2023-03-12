b5103bc172b11e00570c51eae619c7f89c6dbbe7
Elena Martynova <martynova.elena@gmail.com>
2023-03-12 08:31:17 +0300

    changes

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,11 +1,11 @@
 package ru.netology.sqr;
 
 public class SQRService {
-     public int calculateSquare(int lowerdate, int highdate) {
+     public int calculateSquare(int min, int max) {
          int counter = 0;
          for (int i = 10; i <= 99; i++) {
-             if (i * i >= lowerdate) {
-                 if (i * i <= highdate) {
+             if (i * i >= min) {
+                 if (i * i <= max) {
                      counter++;
                  }
              }
